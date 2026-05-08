dad489987c09190571fcc7f9ae2c26e73a41a3c9
HoaiPhuong <hahoaiphuong07012006@gmail.com>
2026-05-08 23:23:44 +0700

    them class moi

---
+++ src/main/java/Test/AppLauncher.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+            // Gọi hàm main của class Main gốc để khởi chạy app
+            Main.main(args);
+    }
+}
